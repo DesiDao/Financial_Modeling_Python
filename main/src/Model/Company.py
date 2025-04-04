6990a11c601e039ff1fb1899ba9b48f7c3d70cd5
DesiDao <55844636+DesiDao@users.noreply.github.com>
2025-04-04 15:04:16 -0400

    Update Company.py

--- main/src/Model/Company.py
+++ main/src/Model/Company.py
@@ -1,1 +1,22 @@
+State
+	long id primary
+	string name
+	float state_tax
+	float income_tax
+	float average_gas_price
 
+Company
+	long id primary
+	string name
+	int total_employee
+	float state_tax
+	float income_tax
+
+Employee
+	long id primary
+	string name
+	float rent
+	long state
+	long employed_with
+	fk state => State.id
+	fk employed_with => Company.id
