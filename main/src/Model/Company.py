babdf8adc604be51aa8cd982630eea9a9647510f
DesiDao <55844636+DesiDao@users.noreply.github.com>
2025-04-07 10:39:19 -0400

    Update Company.py

--- main/src/Model/Company.py
+++ main/src/Model/Company.py
@@ -1,3 +1,4 @@
+""
 State
 	long id primary
 	string name
@@ -20,3 +21,41 @@ Employee
 	long employed_with
 	fk state => State.id
 	fk employed_with => Company.id
+""
+class Company:
+	def __init__(self, total_employee, state_tax, income_tax, id = -1):
+		self.total_employee = total_employee
+		self.state_tax = state_tax
+		self.income_tax = income_tax
+		self.id = -1
+#GET
+	def getTE(self):
+		return self.total_employee
+	def getST(self):
+		return self.state_tax
+	def getIT(self):
+		return self.income_tax
+	def getID(self):
+		return self.id
+#SET
+	def setTE(self, TE):
+		self.total_employee = TE
+	def setST(self, ST):
+		self.state_tax = ST
+	def setIT(self, IT):
+		self.income_tax = IT
+	def setID(self, ID):
+		self.id = ID
+		
+#COMPARE
+	def is_equal(self, other_company):
+	        if not isinstance(other_company, Company):
+	            return False
+	        return (self.total_employee == other_company.getTE() and
+	                self.state_tax == other_company.getST() and
+	                self.income_tax == other_company.getIT() and
+	                self.id == other_company.getID())
+		
+		
+
+
